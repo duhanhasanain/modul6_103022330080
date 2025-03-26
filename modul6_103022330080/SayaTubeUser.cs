83b8e0bf7da7686cfb87b6571c67498cf0be3ffd
duhanhasanain <duhan1325@gmail.com>
2025-03-26 16:02:08 +0700

    “menambahkan dua class SayaTubeVideo dan SayaTubeUser”

---
+++ modul6_103022330080/SayaTubeUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022330080
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        public string Username;
+
+        public SayaTubeUser(string username)
+        {
+            this.Username = username;
+            this.uploadedVideos = new List<SayaTubeVideo>();
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+
+        }
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+
+        }
+
+        public void PrintAllVideoPlayvount()
+        {
+
+        }
+    }
+}
---
+++ modul6_103022330080/SayaTubeVideo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022330080
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string name)
+        {
+
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+
+        }
+
+        public void PrintVideoDetails()
+        {
+
+        }
+    }
+}
